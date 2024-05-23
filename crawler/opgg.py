b7318b1cc029288773036ed33a79495766a8f08f
fanshinglearn <fanshinglearn@gmail.com>
2024-05-23 22:35:11 +0800

    新增 get_team_data

--- crawler/opgg.py
+++ crawler/opgg.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from crawler.crawler_classes import Crawler
+# from crawler_classes import Crawler
 
 class OPGG:
     def __init__(self, lol_id: str):
         self.lol_id = lol_id
-        self.in_game_data = None
-        self.winning_rate = None
+        # self.winning_rate = None
     
     def get_r(self, url):
         headers = {
@@ -25,7 +26,7 @@ class OPGG:
     #             break
         
     #     url = f'https://lol-web-api.op.gg/api/v1.0/internal/bypass/summoners/tw/{self.lol_id}/most-champions/rank?game_type={game_type_en}'
-    #     r = self.get_r(url)
+    #     r = Crawler.get_r(url)
     #     data = r.json()
     #     if data['data']:
     #         win = data['data']['win']
@@ -40,7 +41,7 @@ class Summoner(OPGG):
 
         # https://lol-web-api.op.gg/api/v1.0/internal/bypass/summoners/tw/kAUMc-mNpagwXioYpaRRWtJu9JYxYymfy2kICeyxNMlqciMmK6ES9qq-Sw?hl=zh_TW
         summoner_url = f'https://lol-web-api.op.gg/api/v1.0/internal/bypass/summoners/tw/{self.lol_id}?hl=zh_TW'
-        r = self.get_r(summoner_url)
+        r = Crawler.get_r(summoner_url)
 
         self.summoner_data = r.json()['data']
 
@@ -57,12 +58,12 @@ class InGame(OPGG):
         super().__init__(lol_id)
 
         in_game_url = f'https://lol-web-api.op.gg/api/v1.0/internal/bypass/spectates/tw/{self.lol_id}?hl=zh_TW'
-        r = self.get_r(in_game_url)
+        r = Crawler.get_r(in_game_url)
         
         # 是否在遊戲中
         if r.status_code == 200:
-            self.in_game_data = r.json()['data']
             self.is_in_game = True
+            self.in_game_data = r.json()['data']
 
             # 遊戲 id
             self.game_id = self.in_game_data['game_id']
@@ -71,6 +72,59 @@ class InGame(OPGG):
             self.game_type = self.in_game_data['queue_info']['queue_translate']
         else:
             self.is_in_game = False
+    
+    def get_team_data(self):
+        with open("crawler/test.json", "r", encoding="utf-8") as json_file:
+            self.in_game_data = json.load(json_file)['data']
+
+        participants = self.in_game_data['participants']
+        teams = self.in_game_data['teams']
+
+        new_in_game_data = {
+            'participants': [
+                {
+                    'summoner': {
+                        'summoner_id': p['summoner']['summoner_id'],
+                        'game_name': p['summoner']['game_name'],
+                        'level': p['summoner']['level'],
+                        'previous_tier_info': {
+                            'tier': p['summoner']['previous_seasons'][0]['tier_info']['tier'],
+                            'division': p['summoner']['previous_seasons'][0]['tier_info']['division'],
+                            'tier_image_url': p['summoner']['previous_seasons'][0]['tier_info']['tier_image_url']
+                        } if p['summoner']['previous_seasons'] else {},
+                        'tier_info': {
+                            'tier': p['summoner']['league_stats'][0]['tier_info']['tier'],
+                            'division': p['summoner']['league_stats'][0]['tier_info']['division'],
+                            'lp': p['summoner']['league_stats'][0]['tier_info']['lp'],
+                            'tier_image_url': p['summoner']['league_stats'][0]['tier_info']['tier_image_url'],
+                            'win': p['summoner']['league_stats'][0]['win'],
+                            'lose': p['summoner']['league_stats'][0]['lose']
+                        } if p['summoner']['league_stats'] else {}
+                    },
+                    'team_key': p['team_key'],
+                    'champion_id': p['champion_id'],
+                    'position': p['position']
+                } for p in participants
+            ],
+            'teams': [
+                {
+                    'key': team['key'],
+                    'average_tier_info': {
+                        'tier': team['average_tier_info']['tier'],
+                        'division': team['average_tier_info']['division'],
+                        'tier_image_url': team['average_tier_info']['tier_image_url']
+                    }
+                } for team in teams
+            ]
+        }
+
+        sorted_new_game_data = sorted(new_in_game_data['participants'], key=lambda x: (x['team_key'] != 'BLUE', x['team_key'], ['TOP', 'JUNGLE', 'MID', 'ADC', 'SUPPORT'].index(x['position'])))
+        self.team_data = sorted_new_game_data
+
+        # with open('crawler/test_output.json', 'w', encoding='utf8') as jfile:
+        #     json.dump(self.team_data, jfile, ensure_ascii=False, indent=4)
+
+
 
     def __str__(self) -> str:
         if self.is_in_game:
@@ -93,7 +147,7 @@ class GameData(OPGG):
 
         # https://lol-web-api.op.gg/api/v1.0/internal/bypass/games/tw/summoners/kAUMc-mNpagwXioYpaRRWtJu9JYxYymfy2kICeyxNMlqciMmK6ES9qq-Sw?&limit=20&hl=zh_TW&game_type=total
         game_data_url = f'https://lol-web-api.op.gg/api/v1.0/internal/bypass/games/tw/summoners/{self.lol_id}?&limit={self.limit}&hl=zh_TW&game_type={self.game_type}'
-        r = self.get_r(game_data_url)
+        r = Crawler.get_r(game_data_url)
 
         self.game_data = r.json()['data']
         self.last_game_id = self.game_data[0]['id']
@@ -105,17 +159,23 @@ class GameData(OPGG):
 
 
 if __name__ == '__main__':
+    # 測試時需改成
+    # from crawler_classes import Crawler
+
     # 再吵就送
-    player_id = 'kAUMc-mNpagwXioYpaRRWtJu9JYxYymfy2kICeyxNMlqciMmK6ES9qq-Sw'
+    # player_id = 'kAUMc-mNpagwXioYpaRRWtJu9JYxYymfy2kICeyxNMlqciMmK6ES9qq-Sw'
+    # 中飛妮可
+    player_id = '1nXTkNy9PF9RGpU4P_kFz-49_aze2BUmOXCtJDPOuPsw3_IZY9cRPA2Hlg'
 
     # 是否在遊戲中
     player_in_game = InGame(player_id)
     print(player_in_game)
+    player_in_game.get_participants_data()
 
-    # 玩家資訊
-    player_summoner = Summoner(player_id)
-    print(player_summoner)
+    # # 玩家資訊
+    # player_summoner = Summoner(player_id)
+    # print(player_summoner)
 
-    # 遊戲數據
-    player_game_data = GameData(player_id, limit=1)
-    print(player_game_data)
+    # # 遊戲數據
+    # player_game_data = GameData(player_id, limit=1)
+    # print(player_game_data)
